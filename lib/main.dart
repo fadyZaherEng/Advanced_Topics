556b91a0a049cc7bf5170c5554479f5c2d0655c3
Fady Zaher <fady33zaher@gmail.com>
2023-11-14 10:17:23 +0200

    Advanced Topics

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_topics/src/presentation/screens/home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Advanced Topics',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(title: 'Flutter Advanced Topics'),
+    );
+  }
+}
