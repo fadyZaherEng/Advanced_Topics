35a88ddf4b904d479ebd7057bd5e017edf797097
Fady Zaher <fady33zaher@gmail.com>
2023-11-23 09:54:39 +0200

    splash screen

--- lib/src/config/route/routes_manager.dart
+++ lib/src/config/route/routes_manager.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_advanced_topics/src/presentation/screens/login/login_screen.dart';
 import 'package:flutter_advanced_topics/src/presentation/screens/onBoarding/onboarding_screen.dart';
 
-class Routes {
+class AppRoutes {
   static const String onBoardingScreen = "/onBoardingScreen";
   static const String loginScreen = "loginScreen";
 }
@@ -10,9 +10,9 @@ class Routes {
 class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
-      case Routes.onBoardingScreen:
+      case AppRoutes.onBoardingScreen:
         return _materialRoute(const OnBoardingScreen());
-      case Routes.loginScreen:
+      case AppRoutes.loginScreen:
         return _materialRoute(const LogInScreen());
       default:
         return unDefinedRoute();
---
+++ lib/src/presentation/screens/splash/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_advanced_topics/src/config/route/routes_manager.dart';
+import 'package:flutter_advanced_topics/src/core/resource/image_paths.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToMainScreen();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          ImagePaths.splashScreen,
+        ),
+      ),
+    );
+  }
+
+  void _navigateToMainScreen() {
+    Timer(
+        const Duration(seconds: 5),
+        () => Navigator.pushNamedAndRemoveUntil(
+            context, AppRoutes.onBoardingScreen, (route) => false));
+  }
+}
