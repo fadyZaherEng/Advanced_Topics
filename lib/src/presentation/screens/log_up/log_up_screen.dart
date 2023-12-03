a03364dec95139ff4841ccff9d69e1334c34034a
Fady Zaher <fady33zaher@gmail.com>
2023-12-03 15:08:10 +0200

    logup screen design

---
+++ lib/src/presentation/screens/log_up/log_up_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogUpScreen extends StatefulWidget {
+  const LogUpScreen({super.key});
+
+  @override
+  State<LogUpScreen> createState() => _LogUpScreenState();
+}
+
+class _LogUpScreenState extends State<LogUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
