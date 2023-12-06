db5c4352a332e78ee95f16acc2f692af87aded63
Fady Zaher <fady33zaher@gmail.com>
2023-12-06 11:04:11 +0200

    ..

---
+++ lib/src/core/utils/convert_timestamp_to_date_format.dart
@@ -0,0 +1,20 @@
+import 'package:intl/intl.dart';
+
+String convertTimestampToDateFormat(String timestamp) {
+  if (timestamp.isEmpty) {
+    return "";
+  }
+  DateTime dateTime = DateTime.parse(timestamp);
+
+  String formattedDate = DateFormat('d MMM y').format(dateTime);
+
+  return formattedDate;
+}
+
+String convertTimestampToDateDMYFormat(String timestamp) {
+  DateTime dateTime = DateTime.parse(timestamp);
+
+  String formattedDate = DateFormat('d/M/y').format(dateTime);
+
+  return formattedDate;
+}
---
+++ lib/src/core/utils/ios_date_picker.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Future iosDatePicker({
+  required BuildContext context,
+  required DateTime? selectedDate,
+  required TextEditingController textEditingController,
+  required Function(DateTime) onChange,
+  required Function() onCancel,
+  required Function() onDone,
+}) =>
+    showCupertinoModalPopup(
+        context: context,
+        builder: (context) {
+          return Container(
+            color: Colors.white,
+            height: 300.0,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: onCancel,
+                      child: const Text(
+                        "Cancel",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: onDone,
+                      child: const Text(
+                        "Done",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Flexible(
+                  child: CupertinoDatePicker(
+                    initialDateTime: selectedDate ?? DateTime.now(),
+                    minimumYear: DateTime.now().year,
+                    maximumYear: 2100,
+                    mode: CupertinoDatePickerMode.date,
+                    onDateTimeChanged: onChange,
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
