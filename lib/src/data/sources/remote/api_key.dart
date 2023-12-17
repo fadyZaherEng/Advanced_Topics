dcad51375370de38c3f0b3be34b001030b0e1d71
Fady <fady33zaher@gmail.com>
2023-12-17 13:25:09 +0200

    api login

--- lib/src/data/sources/remote/api_key.dart
+++ lib/src/data/sources/remote/api_key.dart
@@ -1,4 +1,4 @@
-class APIKeys {
+sealed class APIKeys {
   static const String baseUrl = "https://vcare.integration25.com/api/";
   static const String signIn = "auth/login";
 }
--- lib/src/di/data_layer_injector.dart
+++ lib/src/di/data_layer_injector.dart
@@ -2,12 +2,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter_advanced_topics/src/di/injector.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 Future<void> initializeDataDependencies() async {
   final SharedPreferences sharedPreferences =
       await SharedPreferences.getInstance();
   injector.registerLazySingleton(() => Dio()
-    ..options.baseUrl = "APIKeys.baseUrl"
+    ..options.baseUrl = ِAPIKeys.baseUrl
     ..interceptors.add(PrettyDioLogger(
       requestHeader: false,
       requestBody: true,
@@ -16,4 +15,5 @@ Future<void> initializeDataDependencies() async {
       compact: false,
     )));
   injector.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+
 }
