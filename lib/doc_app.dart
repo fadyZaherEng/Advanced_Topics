a02c0d651d75004110b3c35f740c789e0e4d9ca0
Fady <fady33zaher@gmail.com>
2024-01-18 13:19:43 +0200

    no internet

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,18 +1,37 @@
 // ignore_for_file: depend_on_referenced_packages
 
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_topics/generated/l10n.dart';
 import 'package:flutter_advanced_topics/src/config/route/routes_manager.dart';
 import 'package:flutter_advanced_topics/src/config/theme/app_theme.dart';
 import 'package:flutter_advanced_topics/src/di/injector.dart';
 import 'package:flutter_advanced_topics/src/presentation/boc/login/log_in_bloc.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/another_way_to_fix_internet/network_connectivity.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/new_media/add_payment/add_payment_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class DocApp extends StatelessWidget {
+class DocApp extends StatefulWidget {
   const DocApp({super.key});
+
+  @override
+  State<DocApp> createState() => _DocAppState();
+}
+
+class _DocAppState extends State<DocApp> {
+  Map _source = {ConnectivityResult.none: false};
+  final NetworkConnectivity _networkConnectivity = NetworkConnectivity.instance;
+  bool isOnline = true;
+
+  @override
+  void initState() {
+    _internetConnectionListener();
+    super.initState();
+  }
+
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +45,7 @@ class DocApp extends StatelessWidget {
         minTextAdapt: true,
         child: MaterialApp(
           title: 'Doc Doc',
+          navigatorKey: navigatorKey,
           debugShowCheckedModeBanner: false,
           supportedLocales: S.delegate.supportedLocales,
           localizationsDelegates: const [
@@ -41,5 +61,25 @@ class DocApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _internetConnectionListener() {
+    _networkConnectivity.initialise();
+    _networkConnectivity.myStream.listen((source) async {
+      _source = source;
+      switch (_source.keys.toList()[0]) {
+        case ConnectivityResult.mobile:
+          isOnline = _source.values.toList()[0];
+          break;
+        case ConnectivityResult.wifi:
+          isOnline = _source.values.toList()[0];
+          break;
+        case ConnectivityResult.none:
+        default:
+          isOnline = false;
+      }
+      _networkConnectivity.showOrHideNoInternetDialog(
+          isOnline, navigatorKey.currentState!.context);
+    });
   }
 }
