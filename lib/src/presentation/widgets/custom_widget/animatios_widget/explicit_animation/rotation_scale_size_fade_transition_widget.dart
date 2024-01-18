446ec85867beb5bc9f17e91fb83d2335aa0c2f9e
Fady <fady33zaher@gmail.com>
2024-01-18 09:30:24 +0200

    animation

---
+++ lib/src/presentation/widgets/custom_widget/animatios_widget/explicit_animation/rotation_scale_size_fade_transition_widget.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class RotationSizeScaleFadeTransitionWidget extends StatefulWidget {
+  const RotationSizeScaleFadeTransitionWidget({super.key});
+
+  @override
+  State<RotationSizeScaleFadeTransitionWidget> createState() =>
+      _RotationSizeScaleFadeTransitionWidgetState();
+}
+
+class _RotationSizeScaleFadeTransitionWidgetState
+    extends State<RotationSizeScaleFadeTransitionWidget>
+    with SingleTickerProviderStateMixin {
+  AnimationController? controller;
+  Animation<double>? animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat(reverse: true, min: 0, max: 1);
+    animation = CurvedAnimation(
+      parent: controller!,
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        FadeTransition(
+          opacity: animation!,
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+        Text("FadeTransition",
+            style: Theme.of(context).textTheme.headlineSmall),
+        const SizedBox(
+          height: 20,
+        ),
+        SizeTransition(
+          sizeFactor: animation!,
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+        Text("SizeTransition",
+            style: Theme.of(context).textTheme.headlineSmall),
+        const SizedBox(
+          height: 20,
+        ),
+        ScaleTransition(
+          scale: animation!,
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+        Text("ScaleTransition",
+            style: Theme.of(context).textTheme.headlineSmall),
+        const SizedBox(
+          height: 20,
+        ),
+        RotationTransition(
+          turns: animation!,
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+        Text("RotationTransition",
+            style: Theme.of(context).textTheme.headlineSmall),
+      ],
+    );
+  }
+}
