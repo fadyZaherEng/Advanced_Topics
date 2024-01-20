b6e8d1d80fe98e00e06d94f92e3efb92470ae0c9
Fady Zaher <fady33zaher@gmail.com>
2024-01-20 17:17:53 +0200

    notification

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'dart:ui';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_topics/doc_app.dart';
 import 'package:flutter_advanced_topics/src/core/utils/bloc_observer.dart';
 import 'package:flutter_advanced_topics/src/di/injector.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/custom_widget/restart_widget.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/notification_service/local_notification.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/notification_service/notification_services.dart';
 
 import 'firebase_options.dart';
 
@@ -26,6 +29,11 @@ void main() async {
     return true;
   };
   await initializeDependencies();
-  
+  //fcm rest api and local notification and call firebase massaging using api
+  await LocalNotificationService.initialize();
+  await LocalNotificationService.callFirebaseMassaging();
+  //another way to show local notification
+  await NotificationService().initializeNotificationService();
+
   runApp(const RestartWidget(DocApp()));
 }
