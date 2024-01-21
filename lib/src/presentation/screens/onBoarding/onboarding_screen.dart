c3d26a3ea4776f2050d5bd851b0e14dcca4afb73
Fady <fady33zaher@gmail.com>
2024-01-21 11:43:19 +0200

    splash animation Added

--- lib/src/presentation/screens/onBoarding/onboarding_screen.dart
+++ lib/src/presentation/screens/onBoarding/onboarding_screen.dart
@@ -1,9 +1,10 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_topics/src/config/route/routes_manager.dart';
 import 'package:flutter_advanced_topics/src/core/base/widget/base_stateful_widget.dart';
 import 'package:flutter_advanced_topics/src/presentation/screens/onBoarding/widget/onboarding_body_widget.dart';
 import 'package:flutter_advanced_topics/src/presentation/screens/onBoarding/widget/onboarding_logo_widget.dart';
-import 'package:flutter_advanced_topics/src/presentation/widgets/custom_text_field_with_suffix_icon_widget/custom_dropdown_text_field_with_label_widget.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/custom_widget/custom_button_widget.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -16,15 +17,59 @@ class OnBoardingScreen extends BaseStatefulWidget {
 
 class _OnBoardingScreenState extends BaseState<OnBoardingScreen> {
   @override
+  void initState() {
+    super.initState();
+    print("initState");
+  }
+
+  @override
+  void didUpdateWidget(covariant OnBoardingScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("didUpdateWidget");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    print("deactivate");
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print("reassemble");
+  }
+
+  @override
   Widget baseBuild(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsetsDirectional.only(bottom: 20.h, top: 20.h),
+            padding: EdgeInsetsDirectional.only(
+              bottom: 20.h,
+              top: 20.h,
+            ),
             child: Column(
               children: [
-                const OnBoardingLogoWidget(),
+                InkWell(
+                  onTap: () {
+                    setState(() {});
+                  },
+                  child: const OnBoardingLogoWidget(),
+                ),
                 SizedBox(
                   height: 20.h,
                 ),
