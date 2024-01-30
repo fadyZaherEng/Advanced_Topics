d8f61c0d7eb5f1d45e927446d6f8c26a2238208c
Fady <fady33zaher@gmail.com>
2024-01-30 14:20:09 +0200

    flutter Flavors For Android

---
+++ lib/main_production.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_topics/doc_app.dart';
+import 'package:flutter_advanced_topics/src/core/utils/bloc_observer.dart';
+import 'package:flutter_advanced_topics/src/di/injector.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/custom_widget/restart_widget.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/notification_service/local_notification.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/notification_service/notification_services.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  Bloc.observer = const SimpleBlocObserver();
+  await initializeDependencies();
+  //fcm rest api and local notification and call firebase massaging using api
+  await LocalNotificationService.initialize();
+  await LocalNotificationService.callFirebaseMassaging();
+  //another way to show local notification
+  await NotificationService().initializeNotificationService();
+  //flutter crashlytics for crash report
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+  runApp(const RestartWidget(DocApp()));
+}
