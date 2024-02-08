855c083224f2a55e23d1ba398865074b56709568
Fady <fady33zaher@gmail.com>
2024-02-08 15:52:50 +0200

    enhance snack bar style

---
+++ lib/src/presentation/widgets/media/file_picker_image_cropper/cropper_screen.dart
@@ -0,0 +1,8 @@
+class CropperImageScreen extends StatelessWidget {
+  const CropperImageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
