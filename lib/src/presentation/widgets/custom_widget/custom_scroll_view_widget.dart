568d348b30337ab0c85a3edf9f93533e11ba1cbb
Fady <fady33zaher@gmail.com>
2024-02-15 08:49:34 +0200

    sliver and custom scroll view

---
+++ lib/src/presentation/widgets/custom_widget/custom_scroll_view_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_topics/src/core/resource/image_paths.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CustomScrollViewWidget extends StatelessWidget {
+  const CustomScrollViewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: CustomScrollView(
+              shrinkWrap: true,
+              physics: const AlwaysScrollableScrollPhysics(),
+              slivers: [
+                SliverAppBar(
+                  pinned: true,
+                  expandedHeight: 150.0,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: const Text('CustomScrollView'),
+                    background: SvgPicture.asset(ImagePaths.icSuccessNew,
+                        fit: BoxFit.scaleDown),
+                  ),
+                ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return Container(
+                        alignment: Alignment.center,
+                        color: Colors.lightBlue[100 * (index % 9)],
+                        child: Text('List Element $index'),
+                      );
+                    },
+                    childCount: 50,
+                  ),
+                ),
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 20,
+                    child: Text('SliverToBoxAdapter'),
+                  ),
+                ),
+                SliverFillRemaining(
+                  child: Container(
+                    alignment: Alignment.center,
+                    color: Colors.lightBlue,
+                    child: ListView.builder(
+                      itemCount: 50,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          alignment: Alignment.center,
+                          color: Colors.lightBlue[100 * (index % 9)],
+                          child: Text('List Element $index'),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Back'),
+          )
+        ],
+      ),
+    );
+  }
+}
