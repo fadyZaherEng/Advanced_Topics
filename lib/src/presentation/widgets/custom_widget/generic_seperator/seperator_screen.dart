6e42af12fdd6cba94346a6ec1ffb94e34bec1d35
Fady <fady33zaher@gmail.com>
2024-02-25 13:15:20 +0200

    separator

--- lib/src/presentation/widgets/custom_widget/generic_seperator/seperator_screen.dart
+++ lib/src/presentation/widgets/custom_widget/generic_seperator/seperator_screen.dart
@@ -13,23 +13,28 @@ class _SeperatorScreenState extends State<SeperatorScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const Text("Seperator 1"),
-            const Text("Seperator 2"),
-            const Text("Seperator 3"),
-            const Text("Seperator 4"),
-            const Text("Seperator 5"),
-            const Text("Seperator 6"),
-            const Text("Seperator 7"),
-            const Text("Seperator 8"),
-          ]
-              .toAddSeparator(
-                separator: const Divider(
-                  color: Colors.black,
-                ),
-              )
-              .toList(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const Text("Separator 1", style: TextStyle(fontSize: 20)),
+              const Text("Separator 2", style: TextStyle(fontSize: 20)),
+              const Text("Separator 3", style: TextStyle(fontSize: 20)),
+              const Text("Separator 4", style: TextStyle(fontSize: 20)),
+              const Text("Separator 5", style: TextStyle(fontSize: 20)),
+              const Text("Separator 6", style: TextStyle(fontSize: 20)),
+              const Text("Separator 7", style: TextStyle(fontSize: 20)),
+              const Text("Separator 8", style: TextStyle(fontSize: 20)),
+            ]
+                .toAddSeparator(
+                  separator: const SizedBox(
+                    height: 20,
+                  ),
+                )
+                .toList(),
+          ),
         ),
       ),
     );
