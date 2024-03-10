620aab3f4ba0fbaaaf42080f32297a089933e440
Fady <fady33zaher@gmail.com>
2024-03-10 15:19:38 +0200

    check version

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -9,6 +9,7 @@ import 'package:flutter_advanced_topics/src/config/theme/app_theme.dart';
 import 'package:flutter_advanced_topics/src/di/injector.dart';
 import 'package:flutter_advanced_topics/src/presentation/boc/login/log_in_bloc.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/advanced_way_to_fix_internet/network_connectivity.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/custom_widget/quick_action_screen.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/custom_widget/subject_behivour.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/new_media/add_payment/add_payment_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -60,7 +61,7 @@ class _DocAppState extends State<DocApp> {
           //initialRoute: AppRoutes.splashScreen,
           theme: AppTheme("en").light,
           locale: const Locale('en'),
-          home: const SubjectBehivourScreen(),
+          home: const QuickActionScreen(),
         ),
       ),
     );
