19f03a4e0e14f800db2cd58aa98f71cf265f0505
Fady <fady33zaher@gmail.com>
2024-03-10 14:04:01 +0200

    check version

--- lib/src/presentation/widgets/custom_widget/subject_behivour.dart
+++ lib/src/presentation/widgets/custom_widget/subject_behivour.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 class SubjectBehivourScreen extends StatefulWidget {
   const SubjectBehivourScreen({super.key});
@@ -9,6 +10,28 @@ class SubjectBehivourScreen extends StatefulWidget {
 
 class _SubjectBehivourScreenState extends State<SubjectBehivourScreen> {
   //in rx dart
+  final _subject = PublishSubject<int>();
+  final _SubjectBehivour = BehaviorSubject<String>();
+
+  @override
+  void initState() {
+    super.initState();
+    _subject.add(1);
+    _subject.add(2);
+    _subject.add(3);
+    _subject.stream.listen((event) {
+      print(event);
+    }); //
+
+    _SubjectBehivour.add('1');
+    _SubjectBehivour.add('2');
+    _SubjectBehivour.add('3');
+
+    _SubjectBehivour.stream.listen((event) {
+      print(event);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -20,6 +20,7 @@ import firebase_messaging
 import flutter_image_compress_macos
 import flutter_local_notifications
 import flutter_native_timezone
+import package_info_plus
 import path_provider_foundation
 import photo_manager
 import shared_preferences_foundation
@@ -44,6 +45,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
