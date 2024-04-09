8923529f8f7369ca772d06509bd7aa534a025bcb
Fady <fady33zaher@gmail.com>
2024-04-09 22:05:38 +0200

    enhance

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -9,9 +9,9 @@ import 'package:flutter_advanced_topics/src/config/theme/app_theme.dart';
 import 'package:flutter_advanced_topics/src/di/injector.dart';
 import 'package:flutter_advanced_topics/src/presentation/boc/login/log_in_bloc.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/advanced_way_to_fix_internet/network_connectivity.dart';
-import 'package:flutter_advanced_topics/src/presentation/widgets/calender_props/calender_propery.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/media/new_media/add_payment/add_payment_bloc.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/qr_props/badge_identity/badge_identity_bloc/badge_identity_bloc.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/scroll_in_list/scroll_in_list_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -62,7 +62,7 @@ class _DocAppState extends State<DocApp> {
           //initialRoute: AppRoutes.splashScreen,
           theme: AppTheme("en").light,
           locale: const Locale('en'),
-          home: const CalenderPropertyScreen(),
+          home: const ScrollInListScreen(),
         ),
       ),
     );
--- lib/src/presentation/widgets/scroll_in_list/scroll_in_list_screen.dart
+++ lib/src/presentation/widgets/scroll_in_list/scroll_in_list_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_topics/src/config/theme/color_schemes.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class ScrollInListScreen extends StatefulWidget {
   const ScrollInListScreen({super.key});
@@ -8,17 +12,90 @@ class ScrollInListScreen extends StatefulWidget {
 }
 
 class _ScrollInListScreenState extends State<ScrollInListScreen> {
+  final List<Item> _items = [];
+  int _selectedIndex = 0;
+  int seconds = 0;
+
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  void initState() {
+    super.initState();
+    for (var i = 0; i < 5000; i++) {
+      _items.add(Item(GlobalKey(), 'Item $i'));
+      if (i == 4000) _selectedIndex = i;
+    }
+    Future.delayed(const Duration(milliseconds: 500), () {
+      _scrollToIndex(_selectedIndex);
+      Timer.periodic(const Duration(seconds: 1), (timer) {
+        if (mounted) {
+          setState(() {
+            seconds++;
+            if (seconds == 3) {
+              timer.cancel();
+            }
+          });
+        }
+      });
+    });
   }
 
-  Future<void> _scrollToIndex(GlobalKey key) async {
-    Future.delayed(const Duration(milliseconds: 500));
-    Scrollable.ensureVisible(
-      key.currentContext ?? context,
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.easeInOut,
+  final ItemScrollController itemScrollController = ItemScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scroll In List'),
+      ),
+      body: ScrollablePositionedList.builder(
+        itemScrollController: itemScrollController,
+        itemCount: _items.length,
+        itemBuilder: (context, index) {
+          return seconds < 3 && index == _selectedIndex
+              ? Container(
+                  margin: const EdgeInsetsDirectional.symmetric(horizontal: 5),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: ColorSchemes.primary,
+                      width: 2,
+                    ),
+                  ),
+                  child: ListTile(
+                    title: Text(_items[index].title),
+                    onTap: () {
+                      setState(() {
+                        _selectedIndex = index;
+                      });
+                      _scrollToIndex(index);
+                    },
+                  ),
+                )
+              : ListTile(
+                  title: Text(_items[index].title),
+                  onTap: () {
+                    setState(() {
+                      _selectedIndex = index;
+                    });
+                    _scrollToIndex(index);
+                  },
+                );
+        },
+      ),
     );
   }
+
+  Future<void> _scrollToIndex(int index) async {
+    Future.delayed(const Duration(milliseconds: 500));
+    itemScrollController.scrollTo(
+        index: index,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOutCubic);
+    //itemScrollController.jumpTo(index: index);
+  }
+}
+
+class Item {
+  final GlobalKey key;
+  final String title;
+
+  Item(this.key, this.title);
 }
