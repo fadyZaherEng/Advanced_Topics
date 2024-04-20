c1911561ac23d5ca43430a80930c6d1e5d9a65bd
Fady Zaher <fady33zaher@gmail.com>
2024-04-20 16:01:45 +0200

    qr

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -10,6 +10,8 @@ import 'package:flutter_advanced_topics/src/di/injector.dart';
 import 'package:flutter_advanced_topics/src/presentation/boc/login/log_in_bloc.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/advanced_way_to_fix_internet/network_connectivity.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/media/new_media/add_payment/add_payment_bloc.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/otp_all_way/otp_with_timer_auto_fill_and_sms/otp_bloc/otp_bloc.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/qr_props/badge_identity/badge_identity_bloc/badge_identity_bloc.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/qr_props/qr_scan/qr_code_scanner_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
