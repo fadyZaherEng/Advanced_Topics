8577db9f12689fba0e98d18edfbcac22d045ae7a
Fady <fady33zaher@gmail.com>
2024-05-20 10:39:06 +0300

    enhance

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -9,7 +9,7 @@ import 'package:flutter_advanced_topics/src/config/theme/app_theme.dart';
 import 'package:flutter_advanced_topics/src/di/injector.dart';
 import 'package:flutter_advanced_topics/src/presentation/boc/login/log_in_bloc.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/advanced_way_to_fix_internet/network_connectivity.dart';
-import 'package:flutter_advanced_topics/src/presentation/widgets/encrept_decrept/encraption_screen.dart';
+import 'package:flutter_advanced_topics/src/presentation/widgets/button/button_animation/custom_button_animation_new.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/media/new_media/add_payment/add_payment_bloc.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/otp_all_way/otp_with_timer_auto_fill_and_sms/otp_bloc/otp_bloc.dart';
 import 'package:flutter_advanced_topics/src/presentation/widgets/qr_props/badge_identity/badge_identity_bloc/badge_identity_bloc.dart';
@@ -66,7 +66,7 @@ class _DocAppState extends State<DocApp> {
             //initialRoute: AppRoutes.splashScreen,
             theme: AppTheme("en").light,
             locale: const Locale('en'),
-            home: const AESScreen()),
+            home: const CustomButtonAnimationNew()),
       ),
     );
   }
