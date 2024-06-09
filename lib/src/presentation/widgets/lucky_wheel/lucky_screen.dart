8bc8c58092b5f02581449abbf3bdbb223eb57ca0
Fady <fedo.zaher@gmail.com>
2024-06-09 12:30:28 +0300

    fortune wheel

--- lib/src/presentation/widgets/lucky_wheel/lucky_screen.dart
+++ lib/src/presentation/widgets/lucky_wheel/lucky_screen.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: avoid_print
+
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 
@@ -12,6 +15,10 @@ class LuckyWheelScreen extends StatefulWidget {
 
 class _LuckyWheelScreenState extends State<LuckyWheelScreen> {
   StreamController<int> selected = StreamController<int>();
+  bool isRolling = true;
+  int cycleCount = 2;
+  int currentCycle = 0;
+  String currentText = 'Start';
 
   @override
   void dispose() {
@@ -21,7 +28,8 @@ class _LuckyWheelScreenState extends State<LuckyWheelScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final items = <String>[
+    final items =
+    [
       'Grogu',
       'Mace Windu',
       'Obi-Wan Kenobi',
@@ -30,39 +38,74 @@ class _LuckyWheelScreenState extends State<LuckyWheelScreen> {
       'Darth Vader',
       'Yoda',
       'Ahsoka Tano',
+      'Chewbacca',
+      'C-3PO',
+      'R2-D2',
+      'C-3PO',
+      'R2-D2',
+      'C-3PO',
+      'R2-D2',
     ];
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Fortune Wheel'),
+        title: const Text('Fortune Wheel'),
       ),
-      body: GestureDetector(
-        onTap: () {
-          // setState(() {
-          //   selected.add(
-          //     Fortune.randomInt(0, items.length),
-          //   );
-          // });
-        },
-        child: Column(
-          children: [
-            Expanded(
-              child: FortuneWheel(
-                selected: selected.stream,
-                onFling: () {
-                  setState(() {
-                    selected.add(
-                      Fortune.randomInt(0, items.length),
-                    );
-                  });
-                },
-                items: [
-                  for (var it in items) FortuneItem(child: Text(it)),
-                ],
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: GestureDetector(
+              onTap: () {
+                isRolling = true;
+                setState(() {
+                  var random = Fortune.randomInt(0, items.length);
+                  selected.add(random);
+                 // print('Random: $random -> ${items[random]}');
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: FortuneWheel(
+                  selected: selected.stream,
+                  onFling: () {
+                    print('Fling');
+                  },
+                  onAnimationStart: () {
+                    print('Start');
+                  },
+                  onAnimationEnd: () {
+                    print('End');
+                    if (isRolling) {
+                      var random = Fortune.randomInt(0, items.length);
+                      selected.add(random);
+                      print('Random: $random -> ${items[random]}');
+                      currentCycle++;
+                      if (currentCycle == cycleCount) {
+                        setState(() {});
+                        currentText = items[random];
+                        currentCycle = 0;
+                        isRolling = false;
+                        print('Stop');
+                      }
+                    }
+                  },
+                  onFocusItemChanged: (int index) {
+                    print('Focus: $index');
+                  },
+                  items: [
+                    for (var it in items) FortuneItem(child: Text(it)),
+                  ],
+                  animateFirst: false,
+                  // duration: const Duration(seconds: 2),
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 50),
+          Text(currentText),
+          Expanded(flex: 1, child: SizedBox.shrink())
+        ],
       ),
     );
   }
