204160ceb3666764eb603dfadf1312227f70a1f1
Fady <fedo.zaher@gmail.com>
2024-11-19 12:29:13 +0200

    enhance

--- lib/src/presentation/widgets/notification_service/new_with_hms/huwei_developer_website_link.dart
+++ lib/src/presentation/widgets/notification_service/new_with_hms/huwei_developer_website_link.dart
@@ -1,1 +1,6 @@
 //https://developer.huawei.com/consumer/en/service/josp/agc/index.html#/myProject/388421841222035036/97458334310914890?appId=111771547
+
+
+//to get this file
+
+//agconnect-services.json and put it in android -->app as services.json of firebase to configuration
